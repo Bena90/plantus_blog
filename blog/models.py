10986661c5bc1348b6288c9c67cb604e7e0f3983
Bena90 <esteban.benabarre@gmail.com>
2022-09-16 21:13:43 -0300

    Posts added

--- blog/models.py
+++ blog/models.py
@@ -28,6 +28,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
+
+
 class Profile (models.Model):
     user            = models.OneToOneField(User, null=True, on_delete = models.CASCADE)
     description     = models.TextField()
